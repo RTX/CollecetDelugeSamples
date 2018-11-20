c82b9a9f0425b5e6687a33138a6d4d07e202e476
Rotem Or <rotemml@gmail.com>
2018-11-20 19:01:55 +0200

    Single file check

--- app.js
+++ app.js
@@ -1,50 +1,28 @@
-
 var fs = require('fs');
 var parser = require('fast-xml-parser');
-//var he = require('he');
 
-
-
+//var xmlFilePath = "Song008.xml"; //process.argv[2];
+//var delugeFolderPath = "/Users/rotemor/Downloads/Deluge+V2p0p0+factory+card+contents-2";
 
 var xmlFilePath = process.argv[2];
-console.log("xmlFilePath: " + xmlFilePath);
-
-
+var delugeRootFolderPath = process.argv[3];
 var xmlData ="";
 
 fs.readFile(xmlFilePath, function(err, data){
-   // if(err) throw err;
-    //filename must be without .html at the end
     xmlData = data;
-    //console.log(xmlData);
-    var fix ="<top>" + xmlData + "</top>";
-    var jsonObj = parser.parse(fix );
-
-    //var count = jsonObj.top
-    
-
-  // console.log( parser.validate(xmlData));
-    //var jsonObj = parser.parse(xmlData);
-    console.log("-----");
-    
-    for(var i = 0;i <jsonObj.top.song.instruments.kit.length;i++)
-    {
-      for(var y=0;y < jsonObj.top.song.instruments.kit[i].soundSources.sound.length;y++)
-      {
-          console.log(jsonObj.top.song.instruments.kit[i].soundSources.sound[y].osc1.fileName);
-      // console.log(jsonObj.top.song.instruments.kit[i].soundSources.sound[y].osc1.fileName);
-      }
-      console.log("=====");
+    var xmlRootNodeFix ="<root>" + xmlData + "</root>";
+    var jsonObj = parser.parse(xmlRootNodeFix );
+    console.log("----- " + xmlFilePath + " -------------------------");    
+    var instruments =jsonObj.root.song.instruments;
+    for(var i = 0;i < instruments.kit.length;i++){
+      console.log("Missing file on Kit : " +i);
+      for(var y=0;y < instruments.kit[i].soundSources.sound.length;y++){
+        var filenpath = instruments.kit[i].soundSources.sound[y].osc1.fileName;
+        var fullFilePath =  delugeRootFolderPath + "/" + filenpath
+        if (!fs.existsSync(fullFilePath)) {
+           console.log("%10.2f" + fullFilePath );
+          };      
+      };     
     };
-    
+    console.log("End ----- END -----------------------");    
 });
-
- 
-   
-    
-    //var x = xmlDoc.getElementsByTagName("title")[0];
-
- 
-// Intermediate obj
-//var tObj = parser.getTraversalObj(xmlData,options);
-//var jsonObj = parser.convertToJson(tObj,options);
