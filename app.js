e69db7093c80432ae0e652a8cd02ea48a2a09acf
Rotem Or <rotemml@gmail.com>
2018-11-20 19:15:22 +0200

    Single Deluge root folder path Scan songs folder Format output

--- app.js
+++ app.js
@@ -3,26 +3,33 @@ var parser = require('fast-xml-parser');
 
 //var xmlFilePath = "Song008.xml"; //process.argv[2];
 //var delugeFolderPath = "/Users/rotemor/Downloads/Deluge+V2p0p0+factory+card+contents-2";
+var delugeRootFolderPath = process.argv[2];
+var delugeSongsFolder = delugeRootFolderPath + "/SONGS";
 
-var xmlFilePath = process.argv[2];
-var delugeRootFolderPath = process.argv[3];
+
 var xmlData ="";
 
-fs.readFile(xmlFilePath, function(err, data){
-    xmlData = data;
-    var xmlRootNodeFix ="<root>" + xmlData + "</root>";
-    var jsonObj = parser.parse(xmlRootNodeFix );
-    console.log("----- " + xmlFilePath + " -------------------------");    
-    var instruments =jsonObj.root.song.instruments;
-    for(var i = 0;i < instruments.kit.length;i++){
-      console.log("Missing file on Kit : " +i);
-      for(var y=0;y < instruments.kit[i].soundSources.sound.length;y++){
-        var filenpath = instruments.kit[i].soundSources.sound[y].osc1.fileName;
-        var fullFilePath =  delugeRootFolderPath + "/" + filenpath
-        if (!fs.existsSync(fullFilePath)) {
-           console.log("%10.2f" + fullFilePath );
-          };      
-      };     
-    };
-    console.log("End ----- END -----------------------");    
+
+fs.readdirSync(delugeSongsFolder).forEach(file => {
+   fs.readFile(file, function(err, data){
+      xmlData = data;
+      var xmlRootNodeFix ="<root>" + xmlData + "</root>";
+      var jsonObj = parser.parse(xmlRootNodeFix );
+      console.log(file + ":");    
+      var instruments =jsonObj.root.song.instruments;
+      for(var i = 0;i < instruments.kit.length;i++){
+        
+        for(var y=0;y < instruments.kit[i].soundSources.sound.length;y++){
+          var filenpath = instruments.kit[i].soundSources.sound[y].osc1.fileName;
+          var fullFilePath =  delugeRootFolderPath + "/" + filenpath
+          if (!fs.existsSync(fullFilePath)) {
+            console.log("       " + filenpath );
+            };      
+        };     
+      };
+      console.log("----------------------------"); 
+         
+  })
+ 
 });
+console.log("----- END -----------------------"); 
